a12563088b743fe43df6231667f25b618b32859b
wisambbunni <wisambbunni@gmail.com>
2019-01-12 17:48:14 -0500

    Added modal UI to add new tasks

--- index.php
+++ index.php
@@ -14,12 +14,11 @@
 
             <div class="container">
                 <div id="content" class="col s12">
-                    <table class="centered">
+                    <table class="centeamber">
                         <thead>
                             <tr>
-                                <th></th>
-                                <th></th>
-                                <th></th>
+                                <th>Task</th>
+                                <th>Status</th>
                                 <th></th>
                                 <th></th>
                             </tr>
@@ -29,6 +28,43 @@
                         <!-- TODO: Include a PHP script that gets all the elements in todo database -->
                         </tbody>
                     </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="fixed-action-btn">
+            <a class="btn-floating btn-large amber darken-4">
+                <i class="large material-icons">add</i> 
+            </a>
+        </div>
+
+        <div id="add-task-modal" class="modal">
+
+            <div class="modal-content">
+                <h4>Add a Task</h4>
+                <div class="row">
+                    <form action="php/add_task.php" method="post" id="task-add-form" class="col s12">
+
+                        <div class="row modal-form-row">
+                            <div class="input-field col s12">
+                                <input type="text" name="task-name" id="task-id" class="validate" requiamber>
+                                <label for="name">Task Name</label>
+                            </div>
+                        </div>
+
+                        <div class="row modal-form-row">
+                            <div class="input-field col s12">
+                                <input type="text" name="task-name" id="task-id" class="validate" requiamber>
+                                <label for="name">Task Status</label>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                        
+                            <input type="submit" name="enter" class="modal-action waves-effect-waves-amber btn-flat">
+                        </div>
+
+                    </form>
                 </div>
             </div>
         </div>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+    $('.fixed-action-btn').floatingActionButton();
+    $('.modal').modal();
+
+    $('.fixed-action-btn').click(function() {
+        $('#add-task-modal').modal().modal('open');
+    });
 
 });
