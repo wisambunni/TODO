3506db9ed1e6ab7419391c4a3d29239e33b7620a
wisambbunni <wisambbunni@gmail.com>
2019-01-12 20:22:54 -0500

    Modal fix

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     $('.fixed-action-btn').floatingActionButton();
     $('.modal').modal();
+    $('select').formSelect();
 
     $('.fixed-action-btn').click(function() {
         $('#add-task-modal').modal().modal('open');
